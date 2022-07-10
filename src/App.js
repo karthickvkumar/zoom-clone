34c004dc379a58c93a1660c920bd6d0556fa90c4
kvelraj_concord <kvelraj@concord.net>
2022-07-10 17:51:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+    || navigator.mozGetUserMedia; 
+    if(navigator.getUserMedia){
+      navigator.getUserMedia({video: true, audio: true}, (stream) => {
+        console.log(stream)
+        var video = document.getElementById('video-player'); 
+        video.srcObject = stream;
+        video.play();
+        video.muted = true;
+      }, (error) => {
+        console.log(error);
+      })
+    }
+    else{
+      console.log("Browser does not support WebRTC")
+    }
+  }
+
+  render() {
+    const style = {
+      player : {
+        "transform": "rotateY(180deg)",
+        "WebkitTransform": "rotateY(180deg)",
+        "MozTransform": "rotateY(180deg)",
+        "width" : "600px"
+      }
+    }
+
+    return (
+      <div>
+       <video id="video-player" style={style.player}>
+       </video>
+      </div>
+    );
+  }
 }
 
 export default App;
